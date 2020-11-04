6b34382bdc1fd887113fce903beb0e6fd8b8d981
Sam <xiaopeng.luo@mail.utoronto.ca>
2020-11-04 12:51:57 -0500

    Adde event entity and event controller

---
+++ phase1/src/Events/Event.java
@@ -0,0 +1,60 @@
+package Events;
+import java.time.Instant;
+
+
+public class Event {
+    private String speakername;
+    private Instant time;
+    private String eventname;
+    private String[] participants;
+    private String room;
+    private String title;
+    public int capacity;
+    public int id;
+    public static int count = 0;
+
+    public Event(String name, Instant eventtime, String eventname, String[] participants, String room, String title, int capacity ){
+        this.speakername=name;
+        this.time=eventtime;
+        this.eventname=eventname;
+        this.participants=participants;
+        this.room=room;
+        this.title=title;
+        this.capacity=capacity;
+        this.id= Event.count;
+    }
+
+    public String getSpeakername(){
+        return this.speakername;
+    }
+
+    public Instant geteventTime(){
+        return this.time;
+    }
+
+    public String getEventname(){
+        return this.eventname;
+    }
+
+    public String[] getParticipants(){
+        return this.participants;
+    }
+
+    public String getRoom(){
+        return this.room;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+
+}
