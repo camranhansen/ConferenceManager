48c762737af08fd30cfd78c26ba72d76b116bdd5
monique <mmattia909@gmail.com>
2020-11-04 11:13:37 -0500

    Added my messaging tests

--- phase1/Tests/Messaging/MessageManagerTest.java
+++ phase1/Tests/Messaging/MessageManagerTest.java
@@ -1,4 +1,38 @@
 package Messaging;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class MessageManagerTest {
+    @Test(timeout = 50)
+    public void messageManagerConstructTest(){
+        MessageManager messageManager = new MessageManager();
+    }
+
+    @Test(timeout = 50)
+    public void sendMessagesTest(){
+        MessageManager messageManager = new MessageManager();
+        messageManager.sendMessage("potter", "stupefy", "snape");
+        messageManager.sendMessage("snape", "detention", "potter", "weasley", "granger");
+    }
+
+    @Test(timeout = 50)
+    public void retrieveMessagesTest(){
+        MessageManager messageManager = new MessageManager();
+        messageManager.sendMessage("potter", "stupefy", "snape");
+        messageManager.sendMessage("snape", "detention", "potter", "weasley", "granger");
+        HashMap<String, List<Message>> i1 = messageManager.retrieveUserInbox("potter");
+        HashMap<String, List<Message>> i2 = messageManager.retrieveUserInbox("granger");
+        HashMap<String, List<Message>> i3 = messageManager.retrieveUserInbox("snape");
+        assertTrue(i1.containsKey("snape"));
+        assertTrue(i3.containsKey("potter"));
+        String i2Content = i2.get("snape").get(0).getContent();
+        assertEquals("detention", i2Content);
+    }
 }
+
--- phase1/Tests/Messaging/MessageTest.java
+++ phase1/Tests/Messaging/MessageTest.java
@@ -1,4 +1,40 @@
 package Messaging;
 
+import org.junit.Test;
+import java.time.Instant;
+import static org.junit.Assert.*;
+
 public class MessageTest {
+
+    @Test(timeout = 50)
+    public void constructMessageTest() {
+        String[] attendees = {"att1", "att2", "att3"};
+        Message m1 = new Message("org1", attendees, "Hey Guys!");
+        Message m2 = new Message("potter", new String[]{"scooby"}, "Get your hogwarts acceptance yet?");
+    }
+
+    @Test(timeout = 50)
+    public void messageGetContentTest() {
+        Message m2 = new Message("potter", new String[]{"scooby"}, "Get your hogwarts acceptance yet?");
+        assertEquals("Get your hogwarts acceptance yet?", m2.getContent());
+    }
+
+    @Test()
+    public void messageGetTimeTest() {
+        Message m2 = new Message("potter", new String[]{"scooby"}, "Get your hogwarts acceptance yet?");
+        Instant time = m2.getTimeSent();
+    }
+
+    @Test(timeout = 50)
+    public void messageGetSenderTest(){
+        Message m2 = new Message("potter", new String[]{"scooby"}, "Get your hogwarts acceptance yet?");
+        assertEquals("potter", m2.getSender());
+    }
+
+    @Test(timeout = 50)
+    public void messageGetRecipientTest(){
+        String[] attendees = {"att1", "att2", "att3"};
+        Message m1 = new Message("org1", attendees, "Hey Guys!");
+        assertEquals(new String[] {"att1", "att2", "att3"}, m1.getRecipients());
+        assertEquals("att1", m1.getRecipients()[0]);}
 }
