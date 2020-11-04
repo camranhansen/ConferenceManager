a3e30e8b0cb489c963935db40896b27490c73a7d
Harrison Deng <yunyangdeng@gmail.com>
2020-11-04 10:35:11 -0600

    Added test cases for use-case and entities of messaging.

--- phase1/Tests/Messaging/MessageTest.java
+++ phase1/Tests/Messaging/MessageTest.java
@@ -36,5 +36,21 @@ public class MessageTest {
         String[] attendees = {"att1", "att2", "att3"};
         Message m1 = new Message("org1", attendees, "Hey Guys!");
         assertEquals(new String[] {"att1", "att2", "att3"}, m1.getRecipients());
-        assertEquals("att1", m1.getRecipients()[0]);}
+        assertEquals("att1", m1.getRecipients()[0]);
+    }
+
+    public void testMessageSender() {
+        Message message = new Message("Genie", new String[]{"Aladdin"}, "Mr. Aladdin sir, what will your pleasure be?");
+        assertEquals("Check sender.", "Genie", message.getSender());
+    }
+
+    public void testMessageReceiver() {
+        Message message = new Message("Genie", new String[]{"Aladdin"}, "Mr. Aladdin sir, what will your pleasure be?");
+        assertArrayEquals("Check receiver.", new String[]{"Aladdin"}, message.getRecipients());
+    }
+
+    public void testMessageContent() {
+        Message message = new Message("Genie", new String[]{"Aladdin"}, "Mr. Aladdin sir, what will your pleasure be?");
+        assertEquals("Check message content.", "Mr. Aladdin sir, what will your pleasure be?", message.getContent());
+    }
 }
