ee767e6b45b39b7ddc6fa12d4093cf0872a824c0
Alicia Yang <aliciam.yang@mail.utoronto.ca>
2020-11-05 13:49:31 -0500

    Edit writeToEvents

--- phase1/src/Messaging/MessageController.java
+++ phase1/src/Messaging/MessageController.java
@@ -1,5 +1,6 @@
 package Messaging;
 
+import Events.EventManager;
 import Users.UserManager;
 
 import java.lang.reflect.Array;
@@ -31,21 +32,21 @@ public class MessageController {
     public void writeToEvents(String from, String message, int... events) {
         ArrayList<String> recipientsSum = new ArrayList<>();
         for (int i = 0; i < events.length; i++) {
-            recipientsSum.addAll(Arrays.asList(eventManager.viewParticipantsByEvent(events[i])));
+            recipientsSum.addAll(eventManager.getParticipants(events[i]));
         }
         String[] recipients = new String[recipientsSum.size()];
         messageManager.sendMessage(from, message, recipientsSum.toArray(recipients));
     }
 
-    public void orgSendToAllAtt(String from, String message){
-        String[] to = userManager.getAllAtt(); //TODO: Give us a list of people that are "attendees".
-        messageManager.sendMessage(from, message, to);
-    }
+    //public void orgSendToAllAtt(String from, String message){
+      //  String[] to = userManager.getAllAtt(); //TODO: Give us a list of people that are "attendees".
+      //  messageManager.sendMessage(from, message, to);
+   // }
 
-    public void orgSendToAllSpeakers(String from, String message){
-        String[] speakers = userManager.getAllSpeakers(); //TODO: Give us a list of "speakers".
-        messageManager.sendMessage(from, message, speakers);
-    }
+    //public void orgSendToAllSpeakers(String from, String message){
+       // String[] speakers = userManager.getAllSpeakers(); //TODO: Give us a list of "speakers".
+       // messageManager.sendMessage(from, message, speakers);
+    //}
 
     //TODO: Encapsulation of viewing messages.
 }
