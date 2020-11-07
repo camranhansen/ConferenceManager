c37cf4d4fd54981d6b5ec25451d3586cf3a5240b
Sam <xiaopeng.luo@mail.utoronto.ca>
2020-11-06 20:29:28 -0500

    Change all attributes to private and add some setter for event entity. Correct some some error due to above modification.

--- phase1/Tests/Events/EventTest.java
+++ phase1/Tests/Events/EventTest.java
@@ -3,29 +3,34 @@ package Events;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.Instant;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
     @org.junit.jupiter.api.Test
     void getSpeakername() {
         List<String> arr = new ArrayList<>();
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
         assertEquals(e1.getSpeakername(), "Bob Smithers");
     }
 
     @org.junit.jupiter.api.Test
     void getEventTime() {
         List<String> arr = new ArrayList<>();
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
         Instant time = e1.getEventTime();
     }
 
     @org.junit.jupiter.api.Test
     void getEventName() {
         List<String> arr = new ArrayList<>();
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
         assertEquals(e1.getEventName(), "Test Event");
     }
 
@@ -33,30 +38,34 @@ class EventTest {
     void getParticipants() {
         List<String> arr = new ArrayList<>();
         arr.add("Kobe Bryant");
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
         assertEquals(e1.getParticipants().size(),1);
     }
 
     @org.junit.jupiter.api.Test
     void getRoom() {
         List<String> arr = new ArrayList<>();
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
         assertEquals(e1.getRoom(), "Meeting Room 1");
     }
 
     @org.junit.jupiter.api.Test
     void getCapacity() {
         List<String> arr = new ArrayList<>();
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
         assertEquals(e1.getCapacity(), 2);
     }
 
     @org.junit.jupiter.api.Test
     void getId() {
         List<String> arr = new ArrayList<>();
-        Event e1 = new Event("Bob Smithers", "Test Event", arr, "Meeting Room 1",  2);
-        Event e2 = new Event("Kobe Bryant", "Test Event2", arr, "Meeting Room 2",  2);
-        assertEquals(e1.id, 0);
-        assertEquals(e2.id, 1);
+        Instant time = Instant.now();
+        Event e1 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
+        Event e2 = new Event("Bob Smithers",time, "Test Event", arr, "Meeting Room 1",  2);
+        assertEquals(e1.getId(), 0);
+        assertEquals(e2.getId(), 1);
     }
 }
