d224381152ce02ef54749fa935ff07532c050f52
monique <mmattia909@gmail.com>
2020-11-08 11:27:59 -0500

    edited MessagePresenter.

--- phase1/src/Messaging/MessagePresenter.java
+++ phase1/src/Messaging/MessagePresenter.java
@@ -3,11 +3,11 @@ package Messaging;
 public class MessagePresenter {
 
     public void viewOrSend(){
-        System.out.println("to view messages: enter view.\nto send a message: enter send.");
+        System.out.println("to view messages: enter 'view'.\nto send a message: enter 'send'.");
     }
 
     public void viewAllOrFromOne(){
-        System.out.println("to view whole inbox: enter all.\nto view from one username: enter from.");
+        System.out.println("to view whole inbox: enter 'all'.\nto view from one username: enter 'from'.");
     }
 
     public void viewFromOneUsername(){
@@ -15,18 +15,26 @@ public class MessagePresenter {
     }
 
     public void sendToOne(){
+        System.out.println("Would you like to send to one? enter: 'yes' or 'no'");
+    }
+
+    public void enterUsername(){
         System.out.println("enter username to send to:");
     }
 
+    public void enterContent(){
+        System.out.println("enter text:");
+    }
+
     public void sendToAllAttendees(){
-        System.out.println("would you like to send to all attendees? answer: yes or no");
+        System.out.println("would you like to send to all attendees? enter: 'yes' or 'no'");
     }
 
     public void sendToAllSpeakers(){
-        System.out.println("would you like to send to all speakers? answer: yes or no");
+        System.out.println("would you like to send to all speakers? enter: 'yes' or 'no'");
     }
 
     public void sendToEvents(){
-        System.out.println("would you like to send to all your events? answer: yes or no");
+        System.out.println("would you like to send to all your events? enter: 'yes' or 'no'");
     }
 }
