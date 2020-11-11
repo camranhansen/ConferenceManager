22164946fc25ee8c9ffdd87fa421cf98e380b4b1
Camran Hansen <camran.hansen@gmail.com>
2020-11-11 11:00:16 -0500

    Finished MenuControllerTest
    By: Camran

--- phase1/Tests/Menus/MenuControllerTest.java
+++ phase1/Tests/Menus/MenuControllerTest.java
@@ -1,23 +1,38 @@
 package Menus;
 
-import Users.LoginController;
 import Users.Permission;
+import Users.UserController;
 import Users.UserManager;
 import org.junit.Test;
-
+import Users.UserManagerTest;
+import Users.UserControllerTest;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MenuControllerTest {
 
     @Test
     public void selectSubcontroller() {
-        //TODO!!
+        List<Permission> permissions = new ArrayList<>();
+        permissions.add(Permission.MESSAGE_SINGLE_USER);
+        permissions.add(Permission.USER_ALL_EDIT_PERMISSION);
+        HashMap<String,SubController> subcontrollers = new HashMap<>();
+        SubController uc = new UserControllerTest().createUserController();
+        subcontrollers.put("USER",uc);
+        MenuController mc = new MenuController("tim",
+                permissions, subcontrollers);
+
+        String input = "1\n0";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        mc.selectSubcontroller();
+
 
     }
 
