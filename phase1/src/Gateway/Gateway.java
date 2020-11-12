93fa471fa8efcca823f0da80a06cf0484633b44f
Yosi Hatekar <yosi@mobiointeractive.com>
2020-11-12 11:47:47 -0500

    Updated Gateway to have a method to update internal buffer
    Commit by Alicia, Harrison and Yosi:)

--- phase1/src/Gateway/Gateway.java
+++ phase1/src/Gateway/Gateway.java
@@ -3,23 +3,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Gateway {
-    private List<String[]> list;
+    private ArrayList<String[]> buffer;
+    private int colWidth;
+    private String filePath;
 
-    public Gateway(){
-        this.list = new ArrayList<>();
+    public Gateway(int width, String filePath){
+        this.buffer = new ArrayList<>();
+        this.colWidth = width;
+        this.filePath = filePath;
     }
 
-    public List<String[]> getList() {
-        return this.list;
+    public ArrayList<String[]> getBuffer() {
+        return this.buffer;
     }
 
-    public void addToListOfData(String[] input) {
-        if (!this.list.contains(input)) {
-            this.list.add(input);
-        }
-    }
+//    public void addToListOfData(String[] input) {
+//        if (!this.buffer.contains(input)) {
+//            this.buffer.add(input);
+//        }
+//    }
 
     public void remove(String[] input){
-        this.list.remove(input);
+        this.buffer.remove(input);
     }
+
+    public void update(int col, int row, String data){
+        //Updates the internal buffer
+        if (col>colWidth) throw new IllegalArgumentException("Column can not exceed column width in the csv.");
+        if (buffer.size() < row){
+            for (int i = buffer.size(); i < row + 1; i++) {
+                buffer.add(new String[colWidth]); } }
+        buffer.get(row)[col] = data;
+    }
+
+    public void flush(){
+
+    }
+
 }
