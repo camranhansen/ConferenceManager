ae468fa80b57f1b193bf9b9ea64efcaf7a2810d0
Alicia Yang <aliciam.yang@mail.utoronto.ca>
2020-11-12 18:08:24 -0500

    error correction

--- phase1/src/Gateway/EventGateway.java
+++ phase1/src/Gateway/EventGateway.java
@@ -1,12 +1,14 @@
 package Gateway;
 
+import java.io.IOException;
+
 public class EventGateway extends Gateway{
 
     public EventGateway() {
         super(7, "assets/event_data.csv");
     }
 
-    public void readFromGateway(){
+    public void readFromGateway() throws IOException {
         this.readFromFile();
 
     }
