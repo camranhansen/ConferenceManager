00c2c81dd58682a1b9a494fcd4fc5982f773c8ec
Harrison Deng <yunyangdeng@gmail.com>
2020-11-12 11:16:33 -0600

    Finished untested flush and buffered writing to CSV.

--- phase1/src/Gateway/Gateway.java
+++ phase1/src/Gateway/Gateway.java
@@ -1,6 +1,8 @@
 package Gateway;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Gateway {
     private ArrayList<String[]> buffer;
@@ -30,14 +32,50 @@ public class Gateway {
     public void update(int col, int row, String data){
         //Updates the internal buffer
         if (col>colWidth) throw new IllegalArgumentException("Column can not exceed column width in the csv.");
-        if (buffer.size() < row){
+        if (buffer.size() < row) {
             for (int i = buffer.size(); i < row + 1; i++) {
-                buffer.add(new String[colWidth]); } }
+                buffer.add(new String[colWidth]);
+            }
+        }
         buffer.get(row)[col] = data;
     }
 
-    public void flush(){
-
+    public void flush() throws IOException {
+        FileWriter fileWriter = new FileWriter(filePath);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        for (int row = 0; row < buffer.size(); row++) {
+            String[] rowArray = buffer.get(row);
+            for (int col = 0; col < colWidth; col++) {
+                bufferedWriter.write("\"");
+                bufferedWriter.write(rowArray[col]);
+                bufferedWriter.write("\"");
+                if (col != colWidth - 1) {
+                    bufferedWriter.write(",");
+                }
+            }
+            if (row != buffer.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+        bufferedWriter.close();
+        fileWriter.close();
     }
 
+    public void readFromFile(String fileName) {
+        File file = new File(fileName);
+        try {
+            Scanner input = new Scanner(file);
+            while (input.hasNext()) {
+                String string = input.nextLine();
+                String[] a = string.split(",");
+                gateway.addToListOfData(a);
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
