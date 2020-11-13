a639c35ec0281296f6380bcd94234d5a5e5ffc64
Harrison Deng <yunyangdeng@gmail.com>
2020-11-13 17:10:56 -0600

    Fixed message controller tests to use JUnit4.

--- phase1/Tests/Messaging/MessageControllerTest.java
+++ phase1/Tests/Messaging/MessageControllerTest.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 
 public class MessageControllerTest {
 
