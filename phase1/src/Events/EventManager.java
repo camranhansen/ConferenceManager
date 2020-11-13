b424b1767b49b2fea98ebd30a6fa3b358b326a4e
Alicia Yang <aliciam.yang@mail.utoronto.ca>
2020-11-13 12:22:04 -0500

    Update gateway method in eventManager

--- phase1/src/Events/EventManager.java
+++ phase1/src/Events/EventManager.java
@@ -235,14 +235,18 @@ public class EventManager {
         Instant time = Instant.parse(eventData[2]);
         String eventName = eventData[3];
         String participants = eventData[4];
-        String[] listOfParticipants = participants.split(",");
         String room = eventData[5];
         int capacity = Integer.parseInt(eventData[6]);
-        if (!this.events.containsKey(id)){
+        if (!participants.equals("[]")){
+            String participants1 = participants.substring(1, participants.length()-1);
+            String[] list = participants1.split(",");
+            if (!this.events.containsKey(id)){
+                Event event = new Event(speakerName, time, eventName, Arrays.asList(list), room, capacity);
+                this.events.put(id, event);
+            }
+        }else{
             this.createEvent(speakerName, time, eventName, room, capacity);
         }
-
-
 
     }
 
