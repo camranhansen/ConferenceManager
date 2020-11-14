c794bc1bf5b30689a9521e24ec96e27b2909b7a7
Harrison Deng <yunyangdeng@gmail.com>
2020-11-14 12:03:39 -0600

    Added javadocs for gateway.

--- phase1/src/Gateway/Gateway.java
+++ phase1/src/Gateway/Gateway.java
@@ -6,30 +6,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//TODO: if we plan on handling excessively large files, this system will need to change.
+// But We'll be switching to a database then.
+
 public abstract class Gateway {
     private ArrayList<String[]> buffer;
     private int colWidth;
     private String filePath;
 
+    /**
+     * Instantiates a CSV based gateway.
+     * @throws IllegalArgumentException if {@code width} is less than 1, or if {@code filePath} is null.
+     * @param width the width of one row.
+     * @param filePath the path to the file that this gateway handles.
+     */
     public Gateway(int width, String filePath) {
+        if (width < 1) throw new IllegalArgumentException("Width of CSV must not be less than 1.");
+        if (filePath == null) throw new IllegalArgumentException("filePath cannot be null.");
         this.buffer = new ArrayList<>();
         this.colWidth = width;
         this.filePath = filePath;
     }
 
-
+    /**
+     * Returns the value in the CSV file at the given coordinates.
+     * @throws IndexOutOfBoundsException if requested coordinates do not exist currently.
+     * @param row The row to retrieve the cell from indexed starting at 0.
+     * @param col The column to retrieve the cell from indexed starting at 0.
+     * @return A string representing the value stored at that cell.
+     */
     public String getValue(int row, int col) {
-        if (row >= buffer.size()) throw new IndexOutOfBoundsException("Requested row does not exist.");
-        if (col >= colWidth) throw new IndexOutOfBoundsException("Requested column does not exist.");
+        if (row >= buffer.size()) throw new IndexOutOfBoundsException("Requested row (" + row + ") does not exist (greater or equal to: " + buffer.size() + ").");
+        if (col >= colWidth) throw new IndexOutOfBoundsException("Requested column (" + col + ") does not exist (greater or equal to: " + colWidth + ").");
         return buffer.get(row)[col];
     }
 
+    /**
+     * Clears the current buffer.
+     * Note: This does not write to the file. See {@link Gateway#flush()}.
+     */
     public void clearAll() {
         buffer.clear();
     }
 
+    /**
+     * Updates the cell at the given coordinates.
+     * Will generate rows and columns until requested coordinate is reached.
+     * Quotes (") and line breaks (\n) are considered illegal.
+     * Note: This does not write to the file. See {@link Gateway#flush()}.
+     * @throws IllegalArgumentException if any of the previously listed character values exist in {@code data} string.
+     * @param row The row of the cell to write to indexed starting at 0.
+     * @param col The row of the cell to write to indexed starting at 0.
+     * @param data The data to write.
+     */
     public void update(int col, int row, String data) {
         if (data.contains("\n")) throw new IllegalArgumentException("Line breaks not allowed.");
+        if (data.contains("\"")) throw new IllegalArgumentException("Quote (\") characters are not allowed.");
         //Updates the internal buffer
         if (col > colWidth) throw new IllegalArgumentException("Column can not exceed column width in the csv.");
         if (buffer.size() < row + 1) {
@@ -38,7 +70,19 @@ public abstract class Gateway {
         buffer.get(row)[col] = data;
     }
 
+    /**
+     * Updates an entire row of cells. Will generate rows of empty cells until requested row is reached.
+     * Quotes (") and line breaks (\n) are considered illegal.
+     * Note: This does not write to the file. See {@link Gateway#flush()}.
+     * @throws IllegalArgumentException if any of the previously stated illegal characters are included,
+     *                                  or if the data is not equal to the designated amount of columns.
+     * @param row The row coordinator starting from 0.
+     * @param data A {@code String[]} with each index representing a cell in the row. The length of this array must
+     *             equal the designated with of the CSV on instantiation which can be retrieved
+     *             with {@link Gateway#getColWidth()}.
+     */
     public void updateRow(int row, String[] data) {
+        if (data.toString().contains("\"")) throw new IllegalArgumentException("Quote (\") characters are not allowed.");
         if (data.toString().contains("\n")) throw new IllegalArgumentException("Line breaks not allowed.");
         if (data.length != colWidth) throw new IllegalArgumentException("Data is not equal to designated amount of columns.");
         if (buffer.size() < row + 1) {
@@ -51,19 +95,37 @@ public abstract class Gateway {
         }
     }
 
+    /**
+     * Returns an entire row.
+     * @param row The row to be retrieved, starting at 0.
+     * @return a {@code String[]} where each index in the array represents a column in the row.
+     */
     public String[] getRow(int row) {
         if (row >= buffer.size()) throw new IndexOutOfBoundsException("Requested row does not exist.");
         return buffer.get(row);
     }
 
+    /**
+     * @return the width of one row designated on instantiation.
+     */
     public int getColWidth(){
         return colWidth;
     }
 
+    /**
+     * @return the current amount of rows in the buffer.
+     */
     public int getRowCount(){
         return buffer.size();
     }
 
+    /**
+     * Flushes the buffer into the file.
+     * Will create file if it doesn't exist.
+     * Will create parent directories up until the given file path if necessary.
+     * @throws IOException from {@link FileWriter}, {@link FileWriter#close()},
+     *                     {@link BufferedWriter#write(int)} and {@link BufferedWriter#close()}.
+     */
     public void flush() throws IOException {
         File csvFile = new File(filePath);
         csvFile.getParentFile().mkdirs();
@@ -87,6 +149,11 @@ public abstract class Gateway {
         fileWriter.close();
     }
 
+    /**
+     * Reads the data from the file designated at instantiation.
+     * @throws FileNotFoundException if the file does not exist.
+     * @throws IOException from {@link BufferedReader#close()} and {@link FileReader#close()}.
+     */
     public void readFromFile() throws IOException {
         File file = new File(filePath);
         if (!file.isFile()) throw new FileNotFoundException("File does not exist at path: " + file.getAbsolutePath());
@@ -122,6 +189,9 @@ public abstract class Gateway {
         fileReader.close();
     }
 
+    /**
+     * @return the file path designated at instantiation.
+     */
     public String getFilePath() {
         return filePath;
     }
