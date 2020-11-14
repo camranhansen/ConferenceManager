574dd1e1660984bde9b3d0d9bdefe711679dc720
monique <mmattia909@gmail.com>
2020-11-14 13:42:19 -0500

    Added Javadoc to InputPrompter and MessageController

--- phase1/src/Messaging/MessageController.java
+++ phase1/src/Messaging/MessageController.java
@@ -19,6 +19,15 @@ public class MessageController implements SubController {
     private MessagePresenter messagePresenter;
     private InputPrompter inputPrompter;
 
+    /**
+     * Creates a new MessageController with message manger messageManger,
+     *  user manager userManger and event manager eventManger.
+     *
+     * @param messageManager Manger of the messages.
+     * @param userManager Manger of the users.
+     * @param eventManager Manger of the events.
+     */
+
     public MessageController(MessageManager messageManager, UserManager userManager, EventManager eventManager) {
         this.messageManager = messageManager;
         this.userManager = userManager;
@@ -27,6 +36,13 @@ public class MessageController implements SubController {
         this.inputPrompter = new InputPrompter();
     }
 
+    /**
+     * Directs the permission permissionSelected selected by
+     * the user username to further function.
+     *
+     * @param username Username of the current user.
+     * @param permissionSelected Permission the user would like to pursue.
+     */
     public void performSelectedAction(String username, Permission permissionSelected) {
         if (permissionSelected == Permission.MESSAGE_ALL_USERS){
             orgSendToAll(username);
@@ -40,13 +56,19 @@ public class MessageController implements SubController {
         else if (permissionSelected == Permission.VIEW_SELF_MESSAGES){
             viewMessage(username);
         }
-
         else if (permissionSelected == Permission.VIEW_OTHER_MESSAGES){
             String otherUsername = inputPrompter.getResponse("Enter username's messages you'd like to see");
             viewMessage(otherUsername);
         }
     }
 
+    /**
+     * Takes in the message content from the user username.
+     * Gives the user the option to send a message to all attendees or all speakers.
+     * Performs the selected option.
+     *
+     * @param username Username of the current user.
+     */
     public void orgSendToAll(String username){
         String content = getContent();
         Option allAtt = new Option("Send to all attendees"){
@@ -68,6 +90,12 @@ public class MessageController implements SubController {
         choice.run();
     }
 
+    /**
+     * Takes in the username to send a message to.
+     * Takes in content of the message and sends it from from.
+     *
+     * @param from Username of the current user.
+     */
     public void writeMessage(String from) {
         String to = inputPrompter.getResponse("Enter username to send to");
         while(! userManager.userExists(to)){
@@ -78,9 +106,14 @@ public class MessageController implements SubController {
         messageManager.sendMessage(from, content, to);
     }
 
+    /**
+     * Takes in content. Gives the user from the option to send
+     * to all their events or one of them and sends message.
+     *
+     * @param from Username of the current user.
+     */
     public void messageEvents(String from){
         String content = getContent();
-        //TODO: Move option list creation to helper?
         Option allEvents = new Option("Send to all your events"){
             @Override
             public void run(){
@@ -112,6 +145,12 @@ public class MessageController implements SubController {
         choice.run();
     }
 
+    /**
+     * Gives the user username the option to view all their messages or messages
+     * from one other username. Shows them the messages based on their selection.
+     *
+     * @param username Username of the current user.
+     */
     public void viewMessage(String username){
         Option viewAll = new Option("View all your messages"){
             @Override
@@ -135,14 +174,34 @@ public class MessageController implements SubController {
         choice.run();
     }
 
+    /**
+     * Returns the entire inbox of messages of the user username.
+     *
+     * @param username Username of the current user.
+     * @return User's entire inbox of messages.
+     */
     public String viewAllMessages(String username){
         return messageManager.wholeInboxToString(username);
     }
 
+    /**
+     * Returns the messages from from for the username username.
+     *
+     * @param username Username of the current user.
+     * @param from Username of the sender of the messages.
+     * @return User's inbox from specific other username.
+     */
     public String viewMessageFrom(String username, String from) {
         return messageManager.singleInboxToString(username, from);
     }
 
+    /**
+     * Sends a message with content message from from to the events events.
+     *
+     * @param from Username of the current user.
+     * @param message Content of the message
+     * @param events Events to send a message to
+     */
     public void writeToEvents(String from, String message, List<String> events) {
         ArrayList<String> recipientsSum = new ArrayList<>();
         for (int i = 0; i < events.size(); i++) {
@@ -155,6 +214,12 @@ public class MessageController implements SubController {
         else{messagePresenter.noAttInEvent();}
     }
 
+    /**
+     * Sends a message with content message to all attendees from from.
+     *
+     * @param from Username of the current user.
+     * @param message Content of the message.
+     */
     public void orgSendToAllAtt(String from, String message){
         String[] attendees = getStringArray(userManager.getUserByPermissionTemplate(Template.ATTENDEE));
         List<String> list = new ArrayList<>(Arrays.asList(attendees));
@@ -166,6 +231,12 @@ public class MessageController implements SubController {
         else{messagePresenter.noAttendees();}
     }
 
+    /**
+     * Sends a message with content message to all speakers from from.
+     *
+     * @param from Username of the current user.
+     * @param message Content of the message.
+     */
     public void orgSendToAllSpeakers(String from, String message){
         String[] speakers = getStringArray(userManager.getUserByPermissionTemplate(Template.SPEAKER));
         List<String> list = new ArrayList<>(Arrays.asList(speakers));
@@ -188,20 +259,4 @@ public class MessageController implements SubController {
         list.toArray(res);
         return res;
     }
-
-//    private int getValidInput(int options) {
-//        while (true) {
-//            messagePresenter.enterContent();
-//            String input = message    Scanner.nextLine();
-//            if (input.matches("^[0-" + (options + 1) + "]$")) {
-//                return Integer.parseInt(input);
-//            } else {
-//                messagePresenter.errorMessage();
-//            }
-//        }
-//    }
-
-    //TODO: Encapsulation of viewing messages.
-
-
 }
