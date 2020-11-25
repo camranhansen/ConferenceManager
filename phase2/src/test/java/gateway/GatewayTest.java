67df8cfe60ca7d6610056d5a9242ddfe4efd5df8
Harrison Deng <yunyangdeng@gmail.com>
2020-11-25 15:22:13 -0600

    Added test files.

---
+++ phase2/src/test/java/gateway/GatewayTest.java
@@ -0,0 +1,124 @@
+package gateway;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class GatewayTest {
+    private static String testFilePath = "testfiles/test.csv";
+    private Gateway gateway;
+
+    String[][] table = new String[][] {
+            {"Alicia", "Harrison", "Yosi"},
+            {"gateway", "Test", "File"},
+            {"I\'m", "Very", "Bored"}
+    };
+
+    @Before
+    public void createGateway(){
+        gateway = new Gateway(3, testFilePath){};
+    }
+
+    @AfterClass
+    public static void cleanGeneratedFiles() {
+        File testFile = new File(testFilePath);
+        testFile.delete();
+    }
+
+    public ArrayList<String[]> createData(){
+        String[] u1 = {"bob","123", "[MESSAGE_SINGLE_USER]"};
+        String[] u2 = {"louis", "asdf","[MESSAGE_SINGLE_USER, USER_ALL_EDIT_PERMISSION]"};
+        ArrayList<String[]> userList = new ArrayList<>();
+        userList.add(u1);
+        userList.add(u2);
+        return userList;
+    }
+
+    @Test
+    public void testUpdate() {
+        ArrayList<String[]> userList = this.createData();
+
+        Gateway g = gateway;
+        for (int row = 0; row < userList.size(); row++) {
+            String[] u = userList.get(row);
+            g.update(0, row, u[0]);
+            g.update(1, row, u[1]);
+            g.update(2, row, u[2]);
+        }
+
+        for (int row = 0; row < userList.size(); row++) {
+            for (int col = 0; col < userList.get(0).length; col++) {
+                assertEquals(userList.get(row)[col], g.getValue(row, col));
+            }
+        }
+    }
+
+    @Test
+    public void testFlush() throws IOException {
+        Gateway g = gateway;
+        ArrayList<String[]> userList = this.createData();
+        for (int row = 0; row < userList.size(); row++) {
+            String[] u = userList.get(row);
+            g.update(0, row, u[0]);
+            g.update(1, row, u[1]);
+            g.update(2, row, u[2]);
+        }
+        g.flush();
+
+    }
+
+    @Test
+    public void testReadFromFile() {
+        Gateway g = new Gateway(3, "testfiles/testfile.csv") {};
+        try {
+            g.readFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
+                assertEquals(table[row][col], g.getValue(row, col));
+            }
+        }
+    }
+
+    @Test
+    public void testUpdateRow() {
+        for (int row = 0; row < table.length; row++) {
+            gateway.updateRow(row, table[row]);
+        }
+
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
+                assertEquals(table[row][col], gateway.getValue(row, col));
+            }
+        }
+    }
+
+    @Test
+    public void testUpdateRowExisting() {
+        gateway.updateRow(0, table[0]);
+        gateway.updateRow(1, table[2]);
+        gateway.updateRow(0, table[1]);
+
+        assertArrayEquals(table[1], gateway.getRow(0));
+    }
+
+    @Test
+    public void testGetRow() {
+        for (int row = 0; row < table.length; row++) {
+            gateway.updateRow(row, table[row]);
+        }
+
+        for (int row = 0; row < table.length; row++) {
+            assertArrayEquals(table[row], gateway.getRow(row));
+        }
+    }
+}
---
+++ phase2/src/test/java/users/UserControllerTest.java
@@ -0,0 +1,63 @@
+package users;
+
+import org.junit.Test;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    public UserController createUserController(){
+        UserManager um = new UserManagerTest().createUserManager();
+        UserController uc = new UserController(um);
+        return uc;
+    }
+
+    @Test
+    public void performSelectedAction() {
+
+    }
+
+    public void testsetset(UserController uc){
+        System.out.println(uc.selectTemplate().toString());
+    }
+
+
+    @Test
+    public void selectTemplate() {
+        //TODO: This test does not run because of some strange interaction
+        //Between scanner and having it in another file.
+        //It would work if a scanner object was instantiated inside usercontroller
+        String input = "1"+System.lineSeparator()+"1"+System.lineSeparator()+"1"+System.lineSeparator();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        UserController uc = this.createUserController();
+        assertEquals(uc.selectTemplate(), Template.ATTENDEE);
+//        String input2 = "4";
+//        InputStream in2 = new ByteArrayInputStream(input2.getBytes());
+//        System.setIn(in2);
+//        assertEquals(uc.selectTemplate(), Template.ADMIN);
+
+    }
+
+    @Test
+    public void createAccount() {
+        //TODO write these other tests.
+    }
+
+    @Test
+    public void deleteAccount() {
+    }
+
+    @Test
+    public void editPassword() {
+    }
+
+    @Test
+    public void editOtherPassword() {
+    }
+}
