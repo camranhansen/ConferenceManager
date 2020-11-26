9dab8ad15221a56cc34be92f7a56fff93ca513bb
Camran Hansen <camran.hansen@gmail.com>
2020-11-26 11:53:31 -0500

    added event type enum

---
+++ phase2/src/main/java/events/EventType.java
@@ -0,0 +1,7 @@
+package events;
+
+public enum EventType {
+    PARTY, //No speaker
+    MULTI, //Multiple speakers
+    SINGLE, //One speaker
+}
--- phase2/src/main/java/menu/CurrentStateFlag.java
+++ phase2/src/main/java/menu/CurrentStateFlag.java
@@ -8,5 +8,4 @@ public enum CurrentStateFlag {
     BRANCH,
     MAIN,
     LOGIN,
-    ;
 }
