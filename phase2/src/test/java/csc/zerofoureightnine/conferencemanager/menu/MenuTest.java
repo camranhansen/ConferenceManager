a801d915e7aadcd618f572555b869535976e68ee
Camran Hansen <camran.hansen@gmail.com>
2020-11-29 15:20:33 -0500

    Fixed a strange import causing the program to break. Not really sure why this is even a thing. See build #55 console log for more info. import org.omg.CORBA.Current; was somehow added to MenuTest.java.

--- phase2/src/test/java/csc/zerofoureightnine/conferencemanager/menu/MenuTest.java
+++ phase2/src/test/java/csc/zerofoureightnine/conferencemanager/menu/MenuTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.omg.CORBA.Current;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
