0bc856e06f90860fe70c8367a1dc2712f1ed7382
monique <mmattia909@gmail.com>
2020-12-01 01:01:38 -0500

    Added javadoc, mainly to SpecialRequest

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/users/SpecialRequest.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/users/SpecialRequest.java
@@ -8,6 +8,13 @@ public class SpecialRequest {
     private String description;
     private boolean addressed;
 
+    /**
+     * Creates a new SpecialRequest.
+     * @param requestingUser username of the requesting user.
+     * @param header header of this SpecialRequest.
+     * @param description description of this SpecialRequest.
+     * @param addressed Whether or not this SpecialRequest has been addressed.
+     */
     public SpecialRequest(String requestingUser, String header, String description, boolean addressed) {
         this.requestingUser = requestingUser;
         this.header = header;
@@ -16,26 +23,50 @@ public class SpecialRequest {
         this.requestID = UUID.randomUUID();
     }
 
+    /**
+     * Returns the UUID of this SpecialRequest.
+     * @return UUID of this SpecialRequest.
+     */
     public UUID getRequestID() {
         return requestID;
     }
 
+    /**
+     * Returns the username of the user making this SpecialRequest.
+     * @return username of requesting user.
+     */
     public String getRequestingUser() {
         return requestingUser;
     }
 
+    /**
+     * Returns the header of this SpecialRequest.
+     * @return header of this SpecialRequest.
+     */
     public String getHeader() {
         return header;
     }
 
+    /**
+     * Returns the description of this SpecialRequest.
+     * @return description of this SpecialRequest.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns true if this SpecialRequest has been addressed, false if it hasn't.
+     * @return if this SpecialRequest has been addressed.
+     */
     public boolean isAddressed() {
         return addressed;
     }
 
+    /**
+     * Sets the addressed state of this SpecialRequest to addressed.
+     * @param addressed if this SpecialRequest has been addressed.
+     */
     public void setAddressed(boolean addressed) {
         this.addressed = addressed;
     }
