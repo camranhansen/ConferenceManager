52af111a5554e24529fca23f32fdaf0ef397b905
Harrison Deng <yunyangdeng@gmail.com>
2020-12-04 18:52:59 -0600

    Updated javadocs for PersistentMap.

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/gateway/PersistentMap.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/gateway/PersistentMap.java
@@ -26,7 +26,7 @@ public interface PersistentMap<K extends Serializable, V extends Identifiable<K>
      * for the given {@code field}.
      * @param field The field to check the {@code value} for.
      * @param value The value to check for in the {@code field}.
-     * @return
+     * @return The {@see List} of all the data objects that have the {@code value} for the {@code field}.
      */
     List<V> loadForSame(String field, Object value);
 
@@ -34,9 +34,19 @@ public interface PersistentMap<K extends Serializable, V extends Identifiable<K>
      * Loads the data that contains the exact {@code value} in {@code field}'s collection.
      * @param field The field to check the collection for {@code value}.
      * @param value The value to be looked for in {@code field}'s collection.
-     * @return
+     * @return The {@see List} of all the data objects that have the {@code value} contained in the collection of {@code field}.
      */
     List<V> loadInCollection(String field, Object value);
 
+    /**
+     * Loads the data that has {@code search} within the {@code field} value.
+     * Use '%' to denote a wildcard.
+     * For example, if field "sender" has value "Jayden", to search for it:
+     * {@code persistentMap.search("sender", "Jay%")} which will match for anything that has a 
+     * value string value starting with "Jay".
+     * @param field The field to perform the search on.
+     * @param search The {@see String} to search for.
+     * @return A {@see List} that satisfies the search {@See String} in the given {@code field}.
+     */
     List<V> search(String field, String search);
 }
