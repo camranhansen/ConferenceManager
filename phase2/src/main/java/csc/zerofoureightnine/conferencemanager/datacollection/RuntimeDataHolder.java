619a823e88f458e197ff7b2270d649eda11ddad0
klee-oss <57581455+klee-oss@users.noreply.github.com>
2020-12-07 21:49:09 -0800

    Removed redundant functions and updated RuntimeDataHolder  -Kaiser

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/datacollection/RuntimeDataHolder.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/datacollection/RuntimeDataHolder.java
@@ -3,41 +3,18 @@ package csc.zerofoureightnine.conferencemanager.datacollection;
 import csc.zerofoureightnine.conferencemanager.gateway.PersistentMap;
 import csc.zerofoureightnine.conferencemanager.gateway.sql.entities.RuntimeData;
 
-import java.util.EnumMap;
-import java.util.Map;
-
 public class RuntimeDataHolder {
 
-    private EnumMap<RuntimeStats, Integer> statMap;
     private PersistentMap<String, RuntimeData> pMap;
     private String username;
 
     public RuntimeDataHolder(String username, PersistentMap<String, RuntimeData> pMap) {
-        this.statMap = new EnumMap<>(RuntimeStats.class);
-        for (RuntimeStats x: RuntimeStats.values()) { statMap.put(x, 0); }
         this.pMap = pMap;
         this.username = username;
     }
 
-    void incrementStat(RuntimeStats runtimeStat) {
-        statMap.merge(runtimeStat, 1, Integer::sum);
-        pMap.get(username).setStatValue(runtimeStat, statMap.get(runtimeStat));
+    public void incrementStat(RuntimeStats runtimeStat) {
+        int runtimeStatValue = pMap.get(username).getStatValue(runtimeStat) + 1;
+        pMap.get(username).setStatValue(runtimeStat, runtimeStatValue);
     }
-
-    public int getStatValue (RuntimeStats runtimeStats) { return statMap.get(runtimeStats); } // For testing
-
-    @Override
-    //TODO get rid of this function.
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (RuntimeStats x: RuntimeStats.values()) {
-            sb.append(x.toString() + ": " + statMap.get(x).toString() + System.lineSeparator());
-        }
-        return sb.toString();
-    }
-
-    public Map<RuntimeStats, Integer> getStatMap() {
-        return statMap;
-    }
-
 }
