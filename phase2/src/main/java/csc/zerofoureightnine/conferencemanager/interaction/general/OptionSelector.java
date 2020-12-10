3ee84ce3618e061d265ce70a759070eb6e727b1e
Harrison Deng <yunyangdeng@gmail.com>
2020-12-09 23:43:39 -0600

    Fixed issue with attempting to go to back when not possible.

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/interaction/general/OptionSelector.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/interaction/general/OptionSelector.java
@@ -1,23 +1,26 @@
 package csc.zerofoureightnine.conferencemanager.interaction.general;
 
 import java.util.List;
-import java.util.Map;
 
 import csc.zerofoureightnine.conferencemanager.interaction.Action;
 import csc.zerofoureightnine.conferencemanager.interaction.MenuNode;
 import csc.zerofoureightnine.conferencemanager.interaction.Validatable;
-import csc.zerofoureightnine.conferencemanager.users.permission.Permission;
 
+/**
+ * A general {@link Action} and {@link Validatable} meant for choosing between the children
+ * to jump to.
+ */
 public class OptionSelector implements Action, Validatable {
 
     @Override
     public boolean validateInput(String input, List<MenuNode> options) {
-        return input.matches("^[0-9]+$") && Integer.parseInt(input) < options.size();
+        return input.matches("^[0-9]+$") && 
+        Integer.parseInt(input) < options.size() && 
+        options.get(Integer.parseInt(input)) != null;
     }
 
     @Override
-    public MenuNode complete(String username, String input, List<MenuNode> selectableOptions,
-            Map<Permission, MenuNode> selectablePermissions) {
+    public MenuNode complete(String username, String input, List<MenuNode> selectableOptions) {
         return selectableOptions.get(Integer.parseInt(input));
     }
 
