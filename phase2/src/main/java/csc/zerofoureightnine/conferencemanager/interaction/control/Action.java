db0ac17700e41189444a5e0bdc6cebfa39e90444
Harrison Deng <yunyangdeng@gmail.com>
2020-12-11 04:17:11 -0600

    Cleaned up main menu.

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/interaction/control/Action.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/interaction/control/Action.java
@@ -7,14 +7,20 @@ import csc.zerofoureightnine.conferencemanager.interaction.presentation.TopicPre
 public interface Action {
 
     /**
-     * The user is said to have completed an action when this returns. These actions may be simple
-     * menu traversing, or more complex such as sending messages.
-     * @param username the currently logged in users username. May be null if nobody is logged in.
-     * @param input the user input after being validated. May be empty if no input is requested, never null.
-     * @param selectableOptions The options the user has available to them at this point. 0 will 
-     *                          always be main menu, 1 will always be parent. Additionally, parent
-     *                          may be null if the node does not have a parent.
-     * @return An integer representing next node from the list of options to move to.
+     * The user is said to have completed an action when this returns. These actions
+     * may be simple menu traversing, or more complex such as sending messages.
+     * 
+     * @param username          the currently logged in users username. May be null
+     *                          if nobody is logged in.
+     * @param input             the user input after being validated. May be empty
+     *                          if no input is requested, never null.
+     * @param selectableOptions The options the user has available to them at this
+     *                          point. 0 will always be main menu unless action is
+     *                          being ran on main menu, in which case will be null.
+     *                          1 will always be parent. Parent may be null if the
+     *                          node does not have a parent.
+     * @return An integer representing next node from the list of options to move
+     *         to.
      */
     public int complete(String username, String input, List<TopicPresentable> selectableOptions);
 }
