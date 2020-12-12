200a7ba329666ab72349ca7786118eec8d1c8def
Camran Hansen <camran.hansen@gmail.com>
2020-12-11 21:22:20 -0500

    some javadoc

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/datacollection/RuntimeStat.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/datacollection/RuntimeStat.java
@@ -1,9 +1,34 @@
 package csc.zerofoureightnine.conferencemanager.datacollection;
 
+/**
+ * Runtime stats.
+ */
 public enum RuntimeStat {
-    INPUT_RETRIES,
-    MENUS_VISITED,
-    LINES_INPUTTED,
-    COMPLETABLE_COMPLETED,
-    LISTABLE_LISTED,
+    INPUT_RETRIES("Number of retries"),
+    MENUS_VISITED("Number of multi-choice menus visited"),
+    LINES_INPUTTED("Number of lines inputted"),
+    COMPLETABLE_COMPLETED("Number of \"completable\" actions completed"),
+    LISTABLE_LISTED("Number of lists listed");
+
+    private final String renderableText;
+
+    /**
+     * Create a runtimeStat
+     *
+     * @param renderableText the descriptive text for presenting to the user
+     */
+    RuntimeStat(String renderableText) {
+        this.renderableText = renderableText;
+    }
+
+    /**
+     * Get the readable/renderable (essentially not an enum) text
+     *
+     * @return the text
+     */
+    public String getRenderableText() {
+        return renderableText;
+    }
+
+
 }
