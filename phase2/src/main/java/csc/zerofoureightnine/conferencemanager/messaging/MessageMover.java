d7874fdc80f4e13048708ea824d5aa94ca7d4b74
Alicia Yang <aliciam.yang@mail.utoronto.ca>
2020-12-11 21:52:43 -0500

    fixed MessageMover

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/messaging/MessageMover.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/messaging/MessageMover.java
@@ -2,6 +2,7 @@ package csc.zerofoureightnine.conferencemanager.messaging;
 
 import csc.zerofoureightnine.conferencemanager.gateway.sql.entities.MessageData;
 
+import java.time.Instant;
 import java.util.*;
 
 public class MessageMover {
@@ -30,7 +31,8 @@ public class MessageMover {
         this.messageManager.getMessageData().beginInteraction();
         List<MessageData> md = this.messageManager.getMessageData().loadInCollection("recipients", username);
         for (MessageData m: md){
-            if(from.equals(m.getSender())&&content.equals(m.getContent())&&timeSent.equals(m.getTimeSent().toString())){
+            if(from.equals(m.getSender())&&content.equals(m.getContent())&&timeSent.equals
+                    (this.getTime(m.getTimeSent()))){
                 m.removeFromRead(username);
             }
         }
@@ -49,7 +51,8 @@ public class MessageMover {
         this.messageManager.getMessageData().beginInteraction();
         List<MessageData> md = this.messageManager.getMessageData().loadInCollection("recipients", username);
         for (MessageData m: md){
-            if(from.equals(m.getSender())&&content.equals(m.getContent())&&timeSent.equals(m.getTimeSent().toString())){
+            if(from.equals(m.getSender())&&content.equals(m.getContent())&&timeSent.equals
+                    (this.getTime(m.getTimeSent()))){
                 if(!m.getRead().contains(username)){
                     m.addToRead(username);
                 }
@@ -57,6 +60,8 @@ public class MessageMover {
         }
         this.messageManager.getMessageData().endInteraction();
     }
+
+
 
     /**
      * Check if this message is in user's unread inbox, if not, add the given message to this user's archived inbox.
@@ -69,7 +74,7 @@ public class MessageMover {
         List<MessageData> md = this.messageManager.getMessageData().loadInCollection("recipients", username);
         for (MessageData m : md) {
             if (from.equals(m.getSender()) && content.equals(m.getContent()) && timeSent.equals
-                    (m.getTimeSent().toString())) {
+                    (this.getTime(m.getTimeSent()))) {
                 if (!m.getArchived().contains(username)) {
                     m.addToArchived(username);
                 }
@@ -77,6 +82,7 @@ public class MessageMover {
         }
         this.messageManager.getMessageData().endInteraction();
     }
+
 
     /**
      * Remove the given message from user's archived inbox.
@@ -90,12 +96,13 @@ public class MessageMover {
         List<MessageData> md = this.messageManager.getMessageData().loadInCollection("recipients", username);
         for (MessageData m : md) {
             if (from.equals(m.getSender()) && content.equals(m.getContent()) && timeSent.equals
-                    (m.getTimeSent().toString())) {
+                    (this.getTime(m.getTimeSent()))) {
                 m.removeFromArchived(username);
             }
         }
         this.messageManager.getMessageData().endInteraction();
     }
+
 
     /**
      * Delete the given message from user's inbox.
@@ -109,7 +116,7 @@ public class MessageMover {
         List<MessageData> md = this.messageManager.getMessageData().loadInCollection("recipients", username);
         for (MessageData m: md){
             if (from.equals(m.getSender()) && content.equals(m.getContent()) && timeSent.equals
-                    (m.getTimeSent().toString())) {
+                    (this.getTime(m.getTimeSent()))) {
                 m.getRecipients().remove(username);
                 if(m.getRecipients().isEmpty()){
                     this.messageManager.getMessageData().remove(m.getId());
@@ -119,11 +126,12 @@ public class MessageMover {
         this.messageManager.getMessageData().endInteraction();
     }
 
-     /**
-      *Delete all conversations between the user and the given sender from user's inbox.
-      * @param username current user
-      * @param from username of the sender
-      */
+
+        /**
+         * Delete all conversations between the user and the given sender from user's inbox.
+         * @param username current user
+         * @param from username of the sender
+         */
     public void deleteConversation(String username, String from) {
         this.messageManager.getMessageData().beginInteraction();
         List<MessageData> md = this.messageManager.getMessageData().loadInCollection("recipients", username);
@@ -137,6 +145,7 @@ public class MessageMover {
         }
         this.messageManager.getMessageData().endInteraction();
     }
+
 
     /**
      * Clear this user's inbox.
@@ -152,5 +161,13 @@ public class MessageMover {
             }
         }
         this.messageManager.getMessageData().endInteraction();
+    }
+
+    private String getTime(Instant time){
+        StringBuilder string = new StringBuilder();
+        String string1 = time.toString();
+        string.append(string1.substring(0, string1.indexOf(".")));
+        string.append("Z");
+        return string.toString();
     }
 }
