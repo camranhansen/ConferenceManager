33b9b7a2f0ca75c9f13febdb984203c0dfe28644
Camran Hansen <camran.hansen@gmail.com>
2020-12-14 19:54:07 -0500

    small fix

--- phase2/src/main/java/csc/zerofoureightnine/conferencemanager/interaction/presentation/InfoPresentable.java
+++ phase2/src/main/java/csc/zerofoureightnine/conferencemanager/interaction/presentation/InfoPresentable.java
@@ -10,9 +10,9 @@ public interface InfoPresentable {
      * An informative {@link String} formatted that displays potential options for
      * the user currently on this {@see MenuNode}. The options list will be
      * identical to the list passed to
-     * {@link Action#complete(String, String, List, java.util.Map)} except instead
+     * {@link Action#complete(String, String, List)} except instead
      * of {@link MenuNode}, it will be their respective {@link TopicPresentable}s.
-     * 
+     *
      * @param options presentables associated and in the same order as the possible
      *                selections of {@see MenuNode}'s to traverse to.
      * @return The formatted {@link String} with all options displayed to the user.
